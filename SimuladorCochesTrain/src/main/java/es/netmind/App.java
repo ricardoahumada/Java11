f35cf5c346349c6b48d8429cd0a40a114c66f01a
Ricardo <ricardo@enmotionvalue.com>
2022-11-30 12:19:56 +0100

    simulador coches

--- SimuladorCochesTrain/src/main/java/es/netmind/App.java
+++ SimuladorCochesTrain/src/main/java/es/netmind/App.java
@@ -16,6 +16,26 @@ public class App {
         System.out.println(renault + "::" + t_renault);
         renault.parar();
 
+        Coche toyota = new Coche("Toyota", "suv", 110);
+        toyota.encender();
+        double t_toyota = toyota.avanzar(100);
+        System.out.println(toyota + "::" + t_toyota);
+        toyota.parar();
+
+        Coche ferrari = new Coche("Ferrari", "deportivo", 300);
+        ferrari.encender();
+        double t_ferrari = ferrari.avanzar(100);
+        System.out.println(ferrari + "::" + t_ferrari);
+        ferrari.parar();
+
+        if (t_renault < t_ferrari && t_renault < t_toyota) {
+            System.out.println("El ganador es Renault");
+        } else if (t_toyota < t_renault && t_toyota < t_ferrari) {
+            System.out.println("El ganador es Toyota");
+        } else if (t_ferrari < t_renault && t_ferrari < t_toyota) {
+            System.out.println("El más rápido es Ferrari");
+        }
+
         System.out.println("Fin de la simulación!!");
     }
 }
