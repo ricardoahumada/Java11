c4fffa7b593874138625f22632b4f04d262aee74
Ricardo <ricardo@enmotionvalue.com>
2022-11-30 12:04:44 +0100

    simulador coches

--- SimuladorCochesTrain/src/main/java/es/netmind/App.java
+++ SimuladorCochesTrain/src/main/java/es/netmind/App.java
@@ -4,13 +4,18 @@ import es.netmind.models.Coche;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println("Iniciando la simulación ...");
-        System.out.println("Fin la simulación ...");
+
+        Coche renault = new Coche("Renault", "berlina", 120);
+        System.out.println(renault);
+        renault.encender();
+        double t_renault = renault.avanzar(100);
+        System.out.println(renault + "::" + t_renault);
+        renault.parar();
+
+        System.out.println("Fin de la simulación!!");
     }
 }
--- SimuladorCochesTrain/src/main/java/es/netmind/models/Coche.java
+++ SimuladorCochesTrain/src/main/java/es/netmind/models/Coche.java
@@ -7,4 +7,87 @@ public class Coche {
     private boolean encendido = false;
     private double posicion = 0;
 
+    public Coche(String marca, String tipo, int velocidad) {
+        this.marca = marca;
+        this.tipo = tipo;
+        this.velocidad = velocidad;
+    }
+
+    public void encender() {
+        this.encendido = true;
+    }
+
+    public void parar() {
+        this.encendido = false;
+    }
+
+    public double avanzar(int kms) {
+        if (this.encendido) {
+            this.posicion += kms;
+
+            if (this.velocidad != 0) {
+                return ((double) kms / (double) this.velocidad) * penalizar();
+            } else return 99999999;
+        } else {
+            return 0;
+        }
+    }
+
+    private double penalizar() {
+        if (this.tipo.equals("berlina")) return 1;
+        else if (this.tipo.equals("suv")) return 0.9;
+        else if (this.tipo.equals("deportivo")) return 1.1;
+        else return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "marca='" + marca + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", velocidad=" + velocidad +
+                ", encendido=" + encendido +
+                ", posicion=" + posicion +
+                '}';
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
+    public double getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(double posicion) {
+        this.posicion = posicion;
+    }
 }
