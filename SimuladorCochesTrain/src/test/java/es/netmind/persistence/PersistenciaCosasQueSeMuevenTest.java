fde9a961367c490f4615be2841e6e57c142e33a3
Ricardo <ricardo@enmotionvalue.com>
2022-12-02 12:37:13 +0100

    simulador coches

--- SimuladorCochesTrain/src/test/java/es/netmind/persistence/PersistenciaCosasQueSeMuevenTest.java
+++ SimuladorCochesTrain/src/test/java/es/netmind/persistence/PersistenciaCosasQueSeMuevenTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 public class PersistenciaCosasQueSeMuevenTest {
 
+
     @Test
     public void getCosas() {
         PersistenciaCosasQueSeMueven pcqm = PersistenciaCosasQueSeMueven.getInstance();
